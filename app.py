8d7c809641edcb5a610f762dc755d69ac05c5ca9
zbingl <zbingl1234@gmail.com>
2024-02-07 13:13:16 +0100

    working liveplot without refresh

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, send_file
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from matplotlib.figure import Figure
@@ -20,45 +20,45 @@ def read_csv(filename='data.csv'):
             y_vals.append(float(row[1]))
     return x_vals, y_vals
 
+
+
+
+
+
+
 # Function to generate Matplotlib plot
 def generate_plot():
     x, y = read_csv()
     fig = Figure()
     ax = fig.subplots()
-    ax.plot(x, y, marker='o', linestyle='-', color='b')
-    #ax.title('Live Plot from CSV Data')
-    #ax.xlabel('X-axis')
-    #ax.ylabel('Y-axis')
-    #ax.grid(True)
-    #ax.tight_layout()
-    # Save the plot to a temporary file
-    fig.savefig('static/plot.png')
+    ax.plot(x, y, marker='o', linestyle='-', color='g')
+    #fig.savefig('static/plot.png')
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    imdata = f'data:image/png;base64,{base64.b64encode(buf.getbuffer()).decode("ascii")}'
+    print("New plot generated")
+    return imdata
 
-
-    #fig.savefig(buf, format="png")
-    #data = f'data:image/png;base64,{base64.b64encode(buf.getbuffer()).decode("ascii")}'
+data = generate_plot()
 
 # Watchdog event handler to detect changes in the CSV file
 class CSVHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path.endswith('.csv'):
-            generate_plot()
+            data = generate_plot()
 
 # Route to render the HTML template with the live plot
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data = data)
 
-# Route to serve the live plot image
-@app.route('/plot.png')
-def plot_image():
-    return Response(open('static/plot.png', 'rb').read(), mimetype='image/png')
-
-
+@app.route('/get_image')
+def get_image():
+    print("hej")
+    return generate_plot()
 
 if __name__ == '__main__':
-    generate_plot()  # Initial plot generation
-
+    generate_plot()
     # Set up the watchdog observer to monitor changes in the CSV file
     event_handler = CSVHandler()
     observer = Observer()
